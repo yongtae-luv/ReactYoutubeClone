4be457cdcb33b74163ce43f6c696fd754ccc9e71
yongtae-luv <yongtae71@gmail.com>
2021-04-27 12:50:00 +0900

    Video_list create

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDIBqV82lA9oYX7m4msNtplbUv4fYv08mw';
 
 // Create a new component.  This component shold produce 
@@ -18,8 +19,9 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
+      <div> 
         <SearchBar />
+        <VideoList />
        {console.log(this.state.videos)
        }
       </div>
