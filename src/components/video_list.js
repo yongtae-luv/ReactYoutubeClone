ebef7f0bd978519615e00e3d69c1105e976bc8e6
yongtae-luv <yongtae71@gmail.com>
2021-04-27 13:37:45 +0900

    초기파일

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const VideoList = () => {
+const VideoList = (props) => {
   return (
-    <ul>
-      
+    <ul className = "col-md-4 list-group">
+      {props.videos.length}
     </ul>
   )
 }
+
+export default VideoList;
